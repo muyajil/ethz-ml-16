7e27f63918114b17a415eb388ca43e1019c93fc3
Daniel Maag <danielmaag@gmail.com>
2016-10-08 13:49:55 +0200

    merge

--- project1/read_data_old.py
+++ project1/read_data_old.py
@@ -21,24 +21,23 @@ with open(targets_file, "r") as file:
 Y_train = map(int, targets)
 X_train = []
 
-X_length = 0
+first_image = nib.load(os.path.join(root_dir, train_dir, file_prefix_train + str(1) + file_suffix)).get_data()
 
-for i in range(278):
+for i in range(1):#278):
 	image = nib.load(os.path.join(root_dir, train_dir, file_prefix_train + str(i+1) + file_suffix))	
 	(height, width, depth, values) = image.shape
 	data = image.get_data()
 	X = []
 	for a in range(height):
 		for b in range(width):
-			c_vec = [num for sub in data[a][b] for num in sub]
-			c = filter(lambda a: a != 0, c_vec)
-			X.extend(c)
-	if(i == 0):
-		X_length = len(X)
-	else:
-		if(len(X) != X_length):
-			print str(i) + ": Length mismatch!"
-				
+			for c in range(depth):
+				if((first_image[a][b][c][0] > 0) != (data[a][b][c][0] > 0)):
+					print "(" + str(a) + "," + str(b) + "," + str(c) + ")" + " either not both zero or not both bigger than zero"
+					exit()
+				if(data[a][b][c][0] > 0):
+					X.append(data[a][b][c][0])
+					#print data[a][b][c][0]
+
 	X_train.append(X)
 	#print "Finished file " + str(i+1)
 	del image
