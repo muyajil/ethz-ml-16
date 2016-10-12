6376e77c0b1fdd5fcf49c023377ec9db010387ec
dmaag <dmaag@student.ethz.ch>
2016-10-12 16:53:23 +0200

    minimized mem requirements for clean_data

--- project1/clean_data.py
+++ project1/clean_data.py
@@ -1,37 +1,41 @@
 import cPickle
 import sPickle
 
-matrix_train = []
 faulty_line_train = []
-
-matrix_test = []
 faulty_line_test = []
 
-faulty_line_train_num = 14
-faulty_line_test_num = 19
+faulty_line_train_num = 15 #index from actual picture (starts with 1)
+faulty_line_test_num = 20
 
 # Train data
-for elm in sPickle.s_load(open("spickle_train_data.pickle")):
-	matrix_train.append(elm)
-
 for elm in sPickle.s_load(open("spickle_train_data_faulty.pickle")):
 	faulty_line_train.append(elm)
 
-#exchange faulty lines
-matrix_train[faulty_line_train_num] = faulty_line_train
+index = 0
+out_file = open("spickle_train_data_clean.pickle", "w")
+for elm in sPickle.s_load(open("spickle_train_data.pickle")):
+	index += 1
+	if(index == faulty_line_train_num):
+		#exchange faulty lines
+		sPickle.s_dump_elt(faulty_line_train, out_file)
+	else:
+		sPickle.s_dump_elt(elm, out_file)
 
-sPickle.s_dump(matrix_train, open("spickle_train_data_clean.pickle", "w"))
-
-del matrix_train
+del faulty_line_train
+out_file.close()
 
 # Test data
-for elm in sPickle.s_load(open("spickle_test_data.pickle")):
-	matrix_test.append(elm)
-
 for elm in sPickle.s_load(open("spickle_test_data_faulty.pickle")):
 	faulty_line_test.append(elm)
 
-#exchange faulty lines
-matrix_test[faulty_line_test_num] = faulty_line_test
+index = 0
+out_file = open("spickle_test_data_clean.pickle", "w")
+for elm in sPickle.s_load(open("spickle_test_data.pickle")):
+	if(index == faulty_line_test_num):
+		#exchange faulty lines
+		sPickle.s_dump_elt(faulty_line_test, out_file)
+	else:
+		sPickle.s_dump_elt(elm, out_file)
 
-sPickle.s_dump(matrix_train, open("spickle_test_data_clean.pickle", "w"))
+del faulty_line_test
+out_file.close()
