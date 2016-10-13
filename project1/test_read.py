2de3dda875f87b07d27da0caebd207d6c46b954d
Daniel Maag <danielmaag@gmail.com>
2016-10-13 14:48:53 +0200

    meaningfull feedback in test

--- project1/test_read.py
+++ project1/test_read.py
@@ -1,8 +1,19 @@
 import sPickle
 
 matrix = []
+matrix_test = []
+
+total_datapoints = 416
+current_datapoint = 0
 
 for elm in sPickle.s_load(open('spickle_train_data_clean.pickle')):
     matrix.append(elm)
+    current_datapoint += 1
+	print "%.2f" % ((current_datapoint/float(total_datapoints)) * 100) + "%% done"
+
+for elm in sPickle.s_load(open("spickle_test_data_clean.pickle")):
+    matrix_test.append(elm)
+    current_datapoint += 1
+    print "%.2f" % ((current_datapoint/float(total_datapoints)) * 100) + "%% done"
 
 input = input("Just to keep matrix in mem...")
