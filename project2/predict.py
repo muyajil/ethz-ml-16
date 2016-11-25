6fce396307b4a1e466c737647b9c6fe8c6d372e9
Daniel Maag <danielmaag@gmail.com>
2016-11-25 15:24:13 +0100

    some shit donw

--- project2/predict.py
+++ project2/predict.py
@@ -9,12 +9,27 @@ from multiprocessing import Pool
 # Execution flags
 SUBMISSION_VERSION = False # True for final submission -> no output or customizability
 DEBUG = True
-debug_num = 3
+debug_num = 2
+
+# params for aggregating
+MAX = 0
+histogram_bins = 50
+histogram_range = (1, 4001)
 
 # constants
 data_points_train = 278
 data_points_test = 138
 res_folder = "Out/"
+computational_cores = 5
+cube_number = 3
+
+# constants for cutting cube into right shape
+x_start = 20
+x_end = 156
+y_start = 20
+y_end = 188
+z_start = 20
+z_end = 156
 
 def load_img(kind, number):
     img = nib.load("set_" + kind + "/" + kind + "_" + str(number) + ".nii")
@@ -32,27 +47,53 @@ def load_img(kind, number):
 
 def process_img(kind, index):
     X = []
-    X_3d = load_img(kind, index)
+    X_3d = np.array(load_img(kind, index))
 
     # process img and store in 'X'
     # TODO
+    X_3d = X_3d[x_start:x_end, y_start:y_end, z_start:z_end]
+    #print X_3d.shape
 
-    return X
+    XX, YY, ZZ = X_3d.shape
+
+    X_step = XX/cube_number
+    Y_step = YY/cube_number
+    Z_step = ZZ/cube_number
+
+    X = []
+
+    for x in range(cube_number):
+        x_a = x*X_step
+        x_b = min((x+1)*X_step - 1, x_end)
+        for y in range(cube_number):
+            y_a = y*Y_step
+            y_b = min((y+1)*Y_step - 1, y_end)
+            for z in range(cube_number):
+                z_a = z*Z_step
+                z_b = min((z+1)*Z_step - 1, z_end)
+                cube = X_3d[x_a:x_b, y_a:y_b, z_a:z_b]
+                #print str(x_a) + "  " + str(x_b) + "," + str(y_a) + "  " + str(y_b) + "," + str(z_a) + "  " + str(z_b)
+
+                temp = cube.flatten()
+                X.extend(np.histogram(temp, bins=histogram_bins, range=histogram_range)[0])
+    return X # 1D feature vector
 
 def process_img_train(index):
-    X_train = process_img("train", index)
-    print "Finished reading file train_" + str(index) + "; " + "%.2f" % ((index/float(data_points_train)) * 100) + "%"
+    X_train = process_img("train", index[0])
+    print "Finished reading file train_" + str(index[0]) + "; " + "%.2f" % ((index[0]/float(data_points_train)) * 100) + "%"
     return X_train
 
 def process_img_test(index):
-    X_test = process_img("test", index)
-    print "Finished reading file test_" + str(index) + "; " + "%.2f" % ((index/float(data_points_test)) * 100) + "%"
+    X_test = process_img("test", index[0])
+    print "Finished reading file test_" + str(index[0]) + "; " + "%.2f" % ((index[0]/float(data_points_test)) * 100) + "%"
     return X_test
 
 def extract_data(kind):
+    global computational_cores
     image_num = globals()["data_points_" + kind]
     if DEBUG:
         image_num = debug_num
+        computational_cores = min(debug_num, computational_cores)
 
     feature_matrix = []
 
@@ -68,9 +109,9 @@ def extract_data(kind):
         print "No file \'" + file_name + "\' found, starting to read data..."
 
         # parallel reading data
-        feature_matrix = range(1, image_num + 1)
-        p = Pool(5)
-        p.map(globals()["process_img_" + str(kind)], feature_matrix)
+        feature_matrix = [[ls] for ls in range(1, image_num + 1)]
+        p = Pool(computational_cores)
+        feature_matrix = p.map(globals()["process_img_" + str(kind)], feature_matrix)
 
         # write data to file
         out_file = open(out_file, 'w')
@@ -104,6 +145,9 @@ def main():
     # First extract feature matrix from train set and load targets
     X_train = extract_data("train")
     Y_train = read_targets()
+    print len(X_train)
+    # HACK
+    exit()
 
     # TODO Train models
 
